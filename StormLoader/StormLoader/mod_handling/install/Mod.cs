1f74032a3259d8917ca20b7a19b81bff10a2fe13
lewis dixon <lewis.dixon@live.co.uk>
2022-11-10 15:07:34 +0000

    Prepare for in-memory mod install tracking, serialise and deserialise on completed install.

--- StormLoader/StormLoader/mod_handling/install/Mod.cs
+++ StormLoader/StormLoader/mod_handling/install/Mod.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StormLoader.mod_handling.install;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ namespace StormLoader.modder_control_panel
 {
     class Mod
     {
-        
 
+        private List<ModFile> modFiles;
         
         
     }
---
+++ StormLoader/StormLoader/mod_handling/install/ModFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StormLoader.mod_handling.install
+{
+    class ModFile
+    {
+        string name;
+        string path;
+        ModFile overwrites;
+        ModFile overwritten;
+
+
+    }
+}
